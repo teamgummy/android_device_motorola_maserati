61011b4c355b3ef78c10de7ab3db36ed43555f4b
David Hacker <davidhackerdvm@gmail.com>
2012-03-15 22:24:39 -0500

    Use prebuilt spn-conf

--- device_maserati.mk
+++ device_maserati.mk
@@ -222,7 +222,7 @@ PRODUCT_COPY_FILES += \
 # Phone settings
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml \
-    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml \
+    device/motorolamaserati/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml \
 
 
 # Graphics
